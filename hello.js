eb07317809b26f08bcac1d46e6016ca2f127fb97
root <root@TheStash.localdomain>
2017-11-28 21:53:06 +0000

    Initial test commit

---
+++ hello.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(80, '46.101.253.192');
+console.log('Server running at http://APP_PRIVATE_IP_ADDRESS:8080/');
